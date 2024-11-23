32c97ff8d954bf81384db2ce095b0b5319b455be
Shon Barkan <117067935+ShonBarkan@users.noreply.github.com>
2024-11-23 16:28:08 +0200

    [export][mistakes] : add mistakes to xlxs

--- client/src/components/ExsportToExcel/ExsportToExcel.jsx
+++ client/src/components/ExsportToExcel/ExsportToExcel.jsx
@@ -6,7 +6,21 @@ import { DownloadOutlined } from '@ant-design/icons';
 
 
 const ExportToExcel = () => {
-    const {englishTestsScore,mathTestsScore,hebrewTestsScore} = useAppContext()
+    const {englishTestsScore,mathTestsScore,hebrewTestsScore, mistakes} = useAppContext()
+
+    const flattenMistakes = () =>{
+      const flattenedData = [];
+      for (let key in mistakes) {
+        for (let subKey in mistakes[key]) {
+          flattenedData.push({
+            Key: key,
+            SubKey: subKey,
+            Values: mistakes[key][subKey].join(', ') // Joining array values as a string
+          });
+        }
+      }
+      return flattenedData
+    } 
 
     const exportToExcel = () => {
     const wb = XLSX.utils.book_new(); // Create a new workbook
@@ -15,11 +29,14 @@ const ExportToExcel = () => {
     const ws1 = XLSX.utils.aoa_to_sheet(englishTestsScore);
     const ws2 = XLSX.utils.aoa_to_sheet(mathTestsScore);
     const ws3 = XLSX.utils.aoa_to_sheet(hebrewTestsScore);
+    const ws4 = XLSX.utils.json_to_sheet(flattenMistakes());
+
 
     // Append each sheet to the workbook with a name
     XLSX.utils.book_append_sheet(wb, ws1, 'אנגלית');
     XLSX.utils.book_append_sheet(wb, ws2, 'כמותי');
     XLSX.utils.book_append_sheet(wb, ws3, 'מילולי');
+    XLSX.utils.book_append_sheet(wb, ws4, 'טעויות');
 
     // Get the current date, hour, and minute in YYYY-MM-DD_HH-MM format
     const now = new Date();
